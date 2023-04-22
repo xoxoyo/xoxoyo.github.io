82d979de658358d535f95084a78da078707a8e7b
xoyo <xoyo@aliyun.com>
2023-04-22 08:19:41 +0800

    update

---
+++ js/URL_Jump.js
@@ -0,0 +1,20 @@
+function jumpBlog(){
+    window.open('https://juejin.cn/user/3919134183395495');
+}
+
+
+function jumpGithub(){
+    window.open('https://github.com/xoxoyo/');
+}
+
+function jumpWechat(){
+    window.open('../src/page/WeChat.html');
+}
+
+function jumpEmail(){
+    window.open('mailto:xoyo@aliyun.com');
+}
+
+function jumpPay(){
+    window.open('../src/page/Pay.html');
+}
